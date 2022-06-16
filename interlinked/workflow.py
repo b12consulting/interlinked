b16107ee8659804c9d56578d294b2686a49c2626
Bertrand Chenal <bertrand.chenal@engie.com>
2022-06-16 08:55:47 +0200

    Add mutate decorator

--- interlinked/workflow.py
+++ interlinked/workflow.py
@@ -12,6 +12,7 @@ class Item:
         self.fn = None
         self.kw = kw or {}
         self.dependencies = {}
+        self.mutators = {}
 
     def __call__(self, fn):
         self.workflow.by_fn[fn].append(self)
@@ -61,6 +62,13 @@ class Workflow:
             return fn
         return decorator
 
+    def mutate(self, **mutators):
+        def decorator(fn):
+            for item in self.by_fn[fn]:
+                item.mutators = {**mutators, **item.mutators}
+            return fn
+        return decorator
+
     def by_name(self, name):
         '''
         Find a function that match the given name. Either because the
@@ -74,23 +82,27 @@ class Workflow:
         # match contains an extra dict of kw, that contains values
         # used for pattern matching
         item, match_kw = match
-        return item.fn, {**item.kw,  **match_kw}, item.dependencies
+        return item, {**item.kw,  **match_kw}
 
     def run(self, resource_name, **extra_kw):
-        fn, match_kw, dependencies = self.by_name(resource_name)
+        item, match_kw = self.by_name(resource_name)
         kw = {**self.base_kw, **match_kw, **extra_kw}
         # Resolve dependencies
-        if dependencies:
+        if item.dependencies:
             if not self.resolve:
                 raise RuntimeError("Missing resolve function on workflow")
 
-            for alias, table in dependencies.items():
+            for alias, table in item.dependencies.items():
                 table = table.format(**kw)
                 read = bind(self.resolve, [table], kw.copy())
                 kw[alias] = read()
 
+        # Mutate parameters
+        for alias, fn in item.mutators.items():
+            kw[alias] = bind(fn, kw=kw)()
+
         # Run function
-        return bind(fn, kw=kw)()
+        return bind(item.fn, kw=kw)()
 
 
 # Define shortcuts
@@ -98,6 +110,7 @@ default_workflow = Workflow()
 run = default_workflow.run
 provide = default_workflow.provide
 depend = default_workflow.depend
+mutate = default_workflow.mutate
 
 
 
