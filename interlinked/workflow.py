f393125da9c18243beb3ee9ac953ca4b2839355a
Sebastien Wertz <sebastien.wertz@b12-consulting.com>
2024-05-27 09:58:05 +0200

    Make it possible to get multiple product in a single run from workflow.resolve

--- interlinked/workflow.py
+++ interlinked/workflow.py
@@ -191,11 +191,17 @@ class Workflow:
         # used for pattern matching
         return match
 
-    def run(self, resource_name: str, **extra_kw):
+    def run(self, resource_name: str | list[str], **extra_kw):
         """
         Create a Run instance and execute it
         """
-        return Run(self, **extra_kw).resolve(resource_name)
+        if isinstance(resource_name, str):
+            resource_name = [resource_name]
+        run = Run(self, **extra_kw)
+        results = tuple(run.resolve(name) for name in resource_name)
+        if len(results) == 1:
+            return results[0]
+        return results
 
 
 class Run:
